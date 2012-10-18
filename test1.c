e85b3ec583d9be41ec8835700aefa050948a55ad
lin zhangru <easonzhr@gmail.com>
2012-10-18 23:44:01 +0800

    hello

--- test1.c
+++ test1.c
@@ -3,5 +3,18 @@ int main(){
 	int a, b;
 	printf("hello world\n");
 	printf("ok\n");
-	
+	printf("ok\n");
+	printf("ok\n");
+	printf("ok\n");
+	printf("ok\n");
+	printf("ok\n");
+	printf("ok\n");
+	printf("ok\n");
+	printf("ok\n");
+	printf("ok\n");
+	printf("ok\n");
+	printf("ok\n");
+	printf("ok\n");
+	printf("ok\n");
+	printf("ok\n");
 }
